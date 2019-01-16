5f43cdfe0d5a47ff447c3ca3919cafc02afe441b
NGloni <angelonischolar@gmail.com>
2019-01-16 08:19:52 -0500

    json horario

--- TurnosBus/TurnosBus/Controllers/HorarioController.cs
+++ TurnosBus/TurnosBus/Controllers/HorarioController.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TurnosBus.Models;
 
 namespace TurnosBus.Controllers
 {
     public class HorarioController : Controller
     {
+
+        TurnosBusEntities db = new TurnosBusEntities();
+
         // GET: Horario
         public ActionResult Index()
         {
             return View();
+        }
+
+        //[HttpPost]
+        //[AllowAnonymous]
+        public JsonResult getHorarioTable()
+        {
+            try
+            {
+                var fechaList = db.frequencies.Where(f => f.available == 1).ToList<frequency>();
+                List<int> freq=new List<int>();
+                foreach(frequency fecha in fechaList)
+                {
+                    freq.Add((int)fecha.bus.capacity);
+                }
+                return Json(fechaList);
+               
+            }
+            catch (Exception error)
+            {
+                return Json(error);
+            }
+
         }
     }
 }
