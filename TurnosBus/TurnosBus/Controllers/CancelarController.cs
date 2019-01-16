736ef97f1fd01006ef90bea878a165cf5ba3ef63
Jrfranco2 <jonas150397@hotmail.com>
2019-01-16 07:32:51 -0500

    qw

---
+++ TurnosBus/TurnosBus/Controllers/CancelarController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TurnosBus.Models;
+
+namespace TurnosBus.Controllers
+{
+    public class CancelarController : Controller
+    {
+        private TurnosBusEntities db = new TurnosBusEntities();
+
+        // GET: Cancelar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
