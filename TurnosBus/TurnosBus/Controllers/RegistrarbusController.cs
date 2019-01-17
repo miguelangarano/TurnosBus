3310e839ed469bcfd4070d3b737dd3f1954618e2
Jobzi <38593785+Jobzi@users.noreply.github.com>
2019-01-17 08:13:13 -0500

    VistaRegistrarBus

---
+++ TurnosBus/TurnosBus/Controllers/RegistrarbusController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TurnosBus.Controllers
+{
+    public class RegistrarbusController : Controller
+    {
+        // GET: Registrarbus
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
