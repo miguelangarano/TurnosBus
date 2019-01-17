db8c2216d842debe3fa1a54938dff76078135068
Jobzi <38593785+Jobzi@users.noreply.github.com>
2019-01-16 22:05:52 -0500

    Cambios

--- TurnosBus/TurnosBus/App_Start/BundleConfig.cs
+++ TurnosBus/TurnosBus/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace TurnosBus
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Reserva-style.css"));
         }
     }
 }
