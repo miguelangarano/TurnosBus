f9a908115f88f8d291a44b4c6941b90b086e9538
Gabonauta <30781044+Gabonauta@users.noreply.github.com>
2019-01-20 23:00:23 -0500

    3

--- TurnosBus/TurnosBus/Controllers/VerificarController.cs
+++ TurnosBus/TurnosBus/Controllers/VerificarController.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TurnosBus.Models;
 
 namespace TurnosBus.Controllers
 {
     public class VerificarController : Controller
     {
+        private TurnosBusEntities db = new TurnosBusEntities();
         // GET: Verificar
         public ActionResult Index()
         {
-            ViewBag.
-            return View();
+            return View(db.frequencies.ToList());
         }
     }
 }
