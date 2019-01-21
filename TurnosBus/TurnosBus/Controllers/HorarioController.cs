759fd796ab6c30425d1545d9023dbf19f3883caa
miguelangarano <miguelangarano@gmail.com>
2019-01-21 11:37:32 -0500

    Horario commit

--- TurnosBus/TurnosBus/Controllers/HorarioController.cs
+++ TurnosBus/TurnosBus/Controllers/HorarioController.cs
@@ -1,17 +1,54 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TurnosBus.Models;
 
 namespace TurnosBus.Controllers
 {
     public class HorarioController : Controller
     {
+
+        TurnosBusEntities db = new TurnosBusEntities();
+
         // GET: Horario
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public JsonResult getHorarioTable()
+        {
+            try
+            {
+                var fechaList = db.frequencies.Where(f => f.available == true).ToList<frequency>();
+                List<Frecuen> lista = new List<Frecuen>();
+                foreach (frequency l in fechaList)
+                {
+                    Frecuen dummy = new Frecuen() { id = l.id, day = l.day, hour = l.hour.ToString(), available = (bool)l.available, place = l.place.name, capacity = (int)l.bus.capacity };
+                    lista.Add(dummy);
+                }
+                return Json(lista);
+            }
+            catch (Exception error)
+            {
+                return Json(error);
+            }
+
+        }
+    }
+
+    class Frecuen
+    {
+        public int id { get; set; }
+        public string day { get; set; }
+        public string hour { get; set; }
+        public bool available { get; set; }
+        public string place { get; set; }
+        public int capacity { get; set; }
     }
 }
--- TurnosBus/TurnosBus/Scripts/plantilla-script.js
+++ TurnosBus/TurnosBus/Scripts/plantilla-script.js
@@ -4,16 +4,15 @@ function onClickSalir() {
 
 document.getElementById("option1").addEventListener("click", function () {
     console.log("Horarios y Unidades");
+    window.location.assign('/Horario/Index');
 });
 
 document.getElementById("option2").addEventListener("click", function () {
     console.log("Reservar un Turno");
+    //window.location.assign('/Ho');
 });
 
 document.getElementById("option3").addEventListener("click", function () {
-    console.log("Verificar Disponibilidad");
-});
-
-document.getElementById("option4").addEventListener("click", function () {
     console.log("Cancelar Turno");
+    window.location.assign('/Cancelar/Index');
 });
