ee0fc9e53a8e46031e8ec5ab7593f5042a927c75
NGloni <angelonischolar@gmail.com>
2019-01-28 08:14:20 -0500

    Reporte Frequencies
    Reporte de frequencies  completado

---
+++ TurnosBus/TurnosBus/Controllers/ReportController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TurnosBus.Models;
+
+namespace TurnosBus.Controllers
+{
+    public class ReportController : Controller
+    {
+        TurnosBusEntities db = new TurnosBusEntities();
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+      
+
+        [HttpPost]
+        [AllowAnonymous]
+        public JsonResult getFrequencies()
+        {
+            try
+            {
+                var fechaList = db.frequencies.ToList<frequency>();
+                List<FrequenciesTableDetail> lista = new List<FrequenciesTableDetail>();
+                foreach (frequency l in fechaList)
+                {
+                    FrequenciesTableDetail dummy = new FrequenciesTableDetail() { day = l.day, hour = l.hour.ToString(), available = (bool)l.available, plate = l.bus.plate,capacity = (int)l.bus.capacity, place = l.place.name  };
+                    lista.Add(dummy);
+                }
+                return Json(lista);
+            }
+            catch (Exception error)
+            {
+                return Json(error);
+            }
+
+        }
+    }
+
+    class FrequenciesTableDetail
+    {
+        public string day { get; set; }
+        public string hour { get; set; }
+        public bool available { get; set; }
+        public string plate { get; set; }
+        public int capacity { get; set; }
+        public string place { get; set; }
+        
+    }
+}
